6f3d5e73c022040dc42f4ae59fa20f28c15d8313
Brandon Visokay <hello@brandonvisokay.com>
2023-07-18 23:22:30 -0400

    Add a few more routes

--- main.py
+++ main.py
@@ -30,17 +30,39 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-# Get - for testing
-@app.get("/", tags=["test"])
-def greet():
-    return {"Hello": "World"}
+@app.get("/posts/{post_id}", tags=["Posts"], status_code=status.HTTP_200_OK)
+async def read_post(post_id: int, db: db_dependency):
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if post is None:
+        HTTPException(status_code=404, detail="Post was not found")
+    return post
 
-#@app.post("/users/", tags=["quotes_user"])
-@app.post("/users/", status_code=status.HTTP_201_CREATED)
+@app.post("/posts/", tags=["Posts"], status_code=status.HTTP_201_CREATED)
+async def create_post(post: PostBase, db: db_dependency):
+    db_post = models.Post(**post.model_dump())
+    db.add(db_post)
+    db.commit()
+
+@app.delete("/posts/{post_id}", status_code=status.HTTP_200_OK)
+async def delete_post(post_id: int, db: db_dependency):
+    db_post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if db_post is None:
+        raise HTTPException(status_code=404, detail="Post was not found")
+    db.delete(db_post)
+    db.commit()
+
+@app.post("/users/", tags=["Users"], status_code=status.HTTP_201_CREATED)
 async def create_new_user(user: UserBase, db: db_dependency):
     db_user = models.User(**user.model_dump())
     db.add(db_user)
     db.commit()
+
+@app.get("/users/{user_id}", tags=["Users"], status_code=status.HTTP_200_OK)
+async def read_user(user_id: int, db: db_dependency):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
 
         
